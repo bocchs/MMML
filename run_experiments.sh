ec4fb045965681c896114191cacab6b70865200c
Alex Bocchieri <alexb826@gmail.com>
2020-12-13 00:06:20 -0500

    Experimental results

---
+++ run_experiments.sh
@@ -0,0 +1,12 @@
+python knn.py
+python knn.py --PCA
+python knn.py --interp
+
+python svm.py --lam 0
+python svm.py --lam 0.01
+python svm.py --lam 0.1
+python svm.py --lam 1
+python svm.py --lam 10
+
+python neural_network.py
+
